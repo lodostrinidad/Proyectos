2cf42b84ba1df28a9de14476f7fbd891a308464e
lodostrinidad <lodostrinidad@gmail.com>
2024-12-05 21:14:03 +0100

    Delete TFC3 directory

--- TFC3/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer mt-auto py-3 bg-light">
-    <div class="container">
-        <span class="text-muted">© <?php echo date("Y"); ?> Tu Empresa. Todos los derechos reservados.</span>
-    </div>
-</footer>
-</body>
-</html>
